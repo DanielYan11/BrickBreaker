a2be94c68c486e1749fa2951126e4cdd3a25d774
DanielYan11 <100495154+DanielYan11@users.noreply.github.com>
2022-02-26 18:07:08 -0500

    Add files via upload

---
+++ BrickBreakerReal/src/BrickBreak/Main.java
@@ -0,0 +1,20 @@
+package BrickBreak;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		Gameplay game = new Gameplay();
+		frame.setBounds(10, 10, 800, 700);
+		frame.setTitle("Brick Breaker");
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(game);
+		frame.setVisible(true);
+		
+	}
+}
